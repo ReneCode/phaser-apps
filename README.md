a8c0d84d70a3163c26085b21055ea1e075c679ee
HarryMiller <HarryMillerMail@googlemail.com>
2014-09-15 18:10:31 +0200

    collision between two single sprites

--- README.md
+++ README.md
@@ -14,6 +14,18 @@ player = game.add.graphics(0,100);
 player.beginFill(0xddffaa);
 player.drawCircle(0, 0, 20);
 player.endFill();
+
 player.lineStyle(5, 0xf40022);
 player.moveTo(10,10);
 player.lineTo(50,100);
+
+------
+
+myGame.player = game.add.sprite(0,0,'player');
+// handle that sprite in the middle
+myGame.player.anchor.setTo(0.5, 0.5);
+myGame.wall = game.add.sprite(200,0,'wall');
+
+// enable both sprites for collision
+game.physics.enable(myGame.player, Phaser.Physics.ARCADE);
+game.physics.enable(myGame.wall, Phaser.Physics.ARCADE);
--- simple/main.js
+++ simple/main.js
@@ -34,17 +34,48 @@ var preload = function(game) {
     myGame.xyDelta = 2;
     myGame.worldWidth = 2000;
     myGame.worldHeight = 2000;
+
+
+    game.load.image('player', 'assets/circleBlue.png');
+    game.load.image('wall', 'assets/circleA.png');
+
 }
 
 var create = function(game) {
+
+  /*  
     // add a player sprite to give context to the movement
     myGame.player = game.add.graphics(0,100);
     myGame.player.beginFill(0xddffaa);
     myGame.player.drawCircle(0, 0, 20);
     myGame.player.endFill();
-    myGame.player.lineStyle(5, 0xf40022);
-    myGame.player.moveTo(10,10);
-    myGame.player.lineTo(50,100);
+
+    game.physics.arcade.enableBody(myGame.player);
+
+    myGame.wall = game.add.graphics(100,0)
+   // myGame.wall.lineStyle(5, 0xf40022);
+   // myGame.wall.moveTo(0,0);
+   // myGame.wall.lineTo(0,100);
+    myGame.wall.beginFill(0x008888);
+    myGame.wall.drawCircle(0,0,40);
+    myGame.wall.endFill();
+    myGame.wall.enableBody = true;
+*/
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    myGame.player = game.add.sprite(0,0,'player');
+    myGame.player.anchor.setTo(0.5, 0.5);
+//    myGame.player.enableBody = true;
+    myGame.wall = game.add.sprite(200,0,'wall');
+//    myGame.wall.enableBody = true;
+
+    game.physics.enable(myGame.player, Phaser.Physics.ARCADE);
+    game.physics.enable(myGame.wall, Phaser.Physics.ARCADE);
+    myGame.player.body.velocity.x = 5;
+
+
+/*
     
     // set our world size to be bigger than the window so we can move the camera
     game.world.setBounds(-myGame.worldWidth/2, 
@@ -59,9 +90,19 @@ var create = function(game) {
 
     game.camera.width = 200;
 //    myGame.player.x = 0;
+*/
+}
+
+var collision = function(a,b) {
+    console.log("collision:" + a.key, b.key);
+    b.kill();
 }
 
 var update = function(game) {    
+
+    game.physics.arcade.overlap(myGame.player, myGame.wall, collision, null, this);
+
+
     // movement
     if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
       myGame.player.y -= myGame.xyDelta;
@@ -75,7 +116,8 @@ var update = function(game) {
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
       myGame.player.x += myGame.xyDelta;
     }
-    
+
+
 
 }
 
